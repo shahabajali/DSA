5d96cc6c0c4d3c4a22d2e281d3271b886ef70122
Shahabaj ali <shahabajali2003@gmail.com>
2026-07-29 21:28:46 +0530

    Time: 63 ms (15.51%) | Memory: 153.1 MB (16.56%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -5,12 +5,12 @@ public:
       priority_queue<pair<int,int>>pq;
         vector<int>ans;
         for(  int i=0;i<k;i++){
-              pq.push({nums[i], i});
+              pq.push(make_pair(nums[i], i));
         }
       ans.push_back(pq.top().first);
       
         for(  int i = k ; i<n; i++){
-            pq.push({nums[i], i});
+                 pq.push(make_pair(nums[i], i));
            
             while( !pq.empty() && pq.top().second <=( i-k)){
                 pq.pop(); //  outer window value remove

88f00f18a21141a4bf7a7dd6b7c825746e2b19a2
Shahabaj ali <shahabajali2003@gmail.com>
2026-08-03 22:03:23 +0530

    Time: 58 ms (5.56%) | Memory: 53.9 MB (22.69%) - LeetSync

---
+++ 2733-sleep/sleep.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} millis
+ * @return {Promise}
+ */
+function sleep(millis) {
+  return new Promise(resolve => setTimeout(resolve, millis));
+}
+
+// Example
+let t = Date.now();
+sleep(100).then(() => {
+  console.log(Date.now() - t); // ~100
+});
+
+
+/** 
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */

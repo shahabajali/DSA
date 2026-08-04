6eb7bd4f14890fee0810bc89de7b50b5c57431c9
Shahabaj ali <shahabajali2003@gmail.com>
2026-08-04 12:19:11 +0530

    Time: 4 ms (23.00%) | Memory: 11.6 MB (73.28%) - LeetSync

--- 32-longest-valid-parentheses/longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/longest-valid-parentheses.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     int longestValidParentheses(string s) {
-        int n = s.length();
-        int maxLength = 0;
-        stack<int> st;   // stack of indices
-        st.push(-1);     // base index
-
-        for (int i = 0; i < n; i++) {
-            if (s[i] == '(') {
+        int n  = s.length();
+        stack<int>st;
+        int maxlength=  0;
+        st.push(-1);
+        for(  int  i = 0; i<n;i++){
+            if(s[i] =='('){
                 st.push(i);
-            } else {
+            }else{
                 st.pop();
-                if (st.empty()) {
-                    st.push(i);  // reset base
-                } else {
-                    maxLength = max(maxLength, i - st.top());
+                if(st.empty()){
+                    st.push(i);
+                }else{
+                 maxlength =  max(maxlength,i-st.top());
                 }
             }
         }
-        return maxLength;
+        return maxlength;
     }
 };

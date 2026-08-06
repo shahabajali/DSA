280b8b65be804a4fa238c79c5116551aeda98f15
Shahabaj ali <shahabajali2003@gmail.com>
2026-08-06 23:51:48 +0530

    Time: 7 ms (38.03%) | Memory: 23.6 MB (95.53%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -7,8 +7,8 @@ public:
         for(  int i =1 ;i<intervals.size();i++){
 
             // check overflow
-            if( intervals[i][0] <= ans.back()[1]){  //  current idx startin value small ans idx end value 
-                ans.back()[1] = max(ans.back()[1], intervals[i][1]);  // then find max in current idx end value 
+            if( intervals[i][0] <= ans.back()[1]){  //  current idx starting value small   to ans idx end value 
+                ans.back()[1] = max(ans.back()[1], intervals[i][1]);  // then find max in current idx end value  ans ans idx end value
             }else{
                 ans.push_back(intervals[i]);  // push new idx value
             }

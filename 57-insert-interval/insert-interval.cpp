4d2d8f57501e03d5f9f001b44732835ab8b10fd9
Shahabaj ali <shahabajali2003@gmail.com>
2026-08-07 19:03:26 +0530

    Time: 0 ms (100.00%) | Memory: 21.6 MB (76.88%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -3,24 +3,25 @@ public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
         vector<vector<int>>ans;
         for( int i =0 ; i<intervals.size();i++){
-            if(intervals[i][1] < newInterval[0]){
-                ans.push_back(intervals[i]);
+            if(intervals[i][1] < newInterval[0]){ //  inter  i last id small to newinterwal first idx then work other wise not work if cundtion  
+                ans.push_back(intervals[i]);  //min value push
             }
-            else if(intervals[i][0] > newInterval[1] ){
-                ans.push_back(newInterval);
+            else if(intervals[i][0] > newInterval[1] ){ //intervls  i idx big newinterd las idx 
+            // fin min an dpush other wise not work
+                ans.push_back(newInterval);  
             
-            for( int j =i;j<intervals.size();j++){
+            for( int j =i;j<intervals.size();j++){  //push other value
                 ans.push_back(intervals[j]);
 
             }
             return ans;
             }
             else{
-               newInterval[0] = min(newInterval[0] ,intervals[i][0]) ;
-               newInterval[1] = max(newInterval[1] ,intervals[i][1]);
+               newInterval[0] = min(newInterval[0] ,intervals[i][0]) ;  // min
+               newInterval[1] = max(newInterval[1] ,intervals[i][1]);  //max
             }
         }
-         ans.push_back(newInterval);
+         ans.push_back(newInterval); // then loop is complete we have value in new intervals
          return ans;
         
     }

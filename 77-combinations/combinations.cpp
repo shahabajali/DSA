f5b818fb841b66f5b31fc64ae674fa2c803fffb0
Shahabaj ali <shahabajali2003@gmail.com>
2026-08-25 23:29:02 +0530

    Time: 35 ms (90.84%) | Memory: 62.6 MB (73.01%) - LeetSync

--- 77-combinations/combinations.cpp
+++ 77-combinations/combinations.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
-    void helper(int n, int k,vector<vector<int>> &ans,vector<int> &result, int i){
-        if(result.size() == k ){ // check size 2number to
-            ans.push_back(result);     
-            return;  //  not execute  helper noe pop_back result
+    void helper(int n, int k, int start, vector<vector<int>>& result, vector<int>& tem) {
+        if (tem.size() == k) {
+            result.push_back(tem);
+            return;
         }
-       for(int j = i;j<=n;j++){
-        result.push_back(j); // first 1 2       
-        helper(n,k,ans,result,j+1); // 1 2     // number 1
-        result.pop_back();// pop_back number3
-       }
-
+        for (int i = start; i <= n; i++) {
+            tem.push_back(i);
+            helper(n, k, i + 1, result, tem);
+            tem.pop_back();
+        }
     }
+
     vector<vector<int>> combine(int n, int k) {
-        // base case 
-      vector<vector<int>>ans;
-      vector<int>result;
-      helper(n,k,ans,result,1);
-      return ans; 
-        
+        vector<vector<int>> result;   // ✅ Proper initialization
+        vector<int> tem;              // ✅ Proper initialization
+        helper(n, k, 1, result, tem);
+        return result;
     }
 };
